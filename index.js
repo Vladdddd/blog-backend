0f7fd4c0e0aa9cddaf13ed849d7895e05229cf83
Vladdddd <61189724+Vladdddd@users.noreply.github.com>
2022-09-29 18:38:37 +0300

    added Procfile, added uploads folder creation

--- index.js
+++ index.js
@@ -26,7 +26,10 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, "uploads");
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
     cb(null, file.originalname);
